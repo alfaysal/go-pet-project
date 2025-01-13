f1039a680acf1b8f0097a4be93baea715014d737
alfaysal <all.faysal@pathao.com>
2025-01-14 00:34:17 +0600

    implement crud operation in one file.

--- cmd/serve.go
+++ cmd/serve.go
@@ -4,8 +4,13 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/alfaysal/go-pet-project/internal/conn"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"log"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -13,14 +18,117 @@ import (
 var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "A command for serving the application.",
-	Run: func(cmd *cobra.Command, args []string) {
+	PreRun: func(cmd *cobra.Command, args []string) {
 		fmt.Println("serve called")
 		if err := conn.ConnectDefaultDB(); err != nil {
 			fmt.Println(err)
 		}
 	},
+	Run: func(cmd *cobra.Command, args []string) {
+		r := chi.NewRouter()
+		r.Use(middleware.Logger)
+
+		r.Route("/books", func(r chi.Router) {
+			r.Post("/", createBook)
+			r.Get("/", listBooks)
+			r.Get("/{id}", getBook)
+			r.Put("/{id}", updateBook)
+			//r.Delete("/{id}", deleteBook)
+		})
+
+		log.Fatal(http.ListenAndServe(":8099", r))
+	},
 }
 
 func init() {
 	rootCmd.AddCommand(serveCmd)
+}
+
+type Book struct {
+	Id     int    `json:"id" gorm:"primaryKey"`
+	Name   string `json:"name"`
+	Author string `json:"author"`
+}
+
+func listBooks(w http.ResponseWriter, r *http.Request) {
+	var books []Book
+	w.Header().Set("Content-Type", "application/json")
+
+	DB := conn.GetDefaultDB()
+	if err := DB.Find(&books).Error; err != nil {
+		fmt.Println(err)
+	}
+
+	err := json.NewEncoder(w).Encode(books)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	w.Header().Set("Content-Type", "application/json")
+	id := chi.URLParam(r, "id")
+
+	DB := conn.GetDefaultDB()
+
+	if err := DB.First(&book, id).Error; err != nil {
+		fmt.Println(err)
+	}
+
+	err := json.NewEncoder(w).Encode(book)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	w.Header().Set("Content-Type", "application/json")
+
+	DB := conn.GetDefaultDB()
+
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		fmt.Println(err)
+	}
+
+	if err := DB.Create(&book).Error; err != nil {
+		fmt.Println(err)
+	}
+
+	err := json.NewEncoder(w).Encode(book)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	w.Header().Set("Content-Type", "application/json")
+
+	id := chi.URLParam(r, "id")
+
+	DB := conn.GetDefaultDB()
+
+	if err := DB.First(&book, id).Error; err != nil {
+		fmt.Println(err)
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		fmt.Println(err)
+	}
+
+	if err := DB.Save(&book).Error; err != nil {
+		fmt.Println(err)
+	}
+
+	err := json.NewEncoder(w).Encode(book)
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
